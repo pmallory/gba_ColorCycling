50a9b231c484093d99dc3d7b85eaf1e0d252bf8d
pmallory <philip@pmallory.com>
2014-03-04 23:26:04 -0500

    generate 16 bit gameboy colors

--- paletteMaker.py
+++ paletteMaker.py
@@ -1,7 +1,8 @@
 import math
 
 def rgb2Color(red, green, blue):
-    return "{0:02x}{1:02x}{2:02x}".format(int(red), int(green), int(blue))
+    color = int(blue)<<10 | int(green)<<5 | int(red)
+    return "{0:02x}".format(color)
 
 def makeGradient(freq1, freq2, freq3,
                  phase1, phase2, phase3,
@@ -9,15 +10,15 @@ def makeGradient(freq1, freq2, freq3,
     """
     adapted from http://krazydad.com/tutorials/makecolors.php
     """
-    with open('out.html', 'w') as f:
+    with open('pal.c', 'w') as f:
         f.write('')
 
-    with open('out.html', 'a') as f:
+    with open('pal.c', 'a') as f:
         for i in xrange(length):
             red = math.sin(freq1*i + phase1) * width + center
             green = math.sin(freq2*i + phase2) * width + center
             blue = math.sin(freq3*i + phase3) * width + center
 
-            f.write('<font color="#{}">&#9608;</font>'.format(rgb2Color(red, green, blue)))
+            f.write('0x{},'.format(rgb2Color(red, green, blue)))
 
-makeGradient(.04,.04,.04,0,2,4,128,127,150)
+makeGradient(.04,.04,.04,0,2,4,16,15,150)
